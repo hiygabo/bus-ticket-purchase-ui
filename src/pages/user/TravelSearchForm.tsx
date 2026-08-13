54eeb8804eb05a8c6a922fd0d2065984b67df962
hiygabo <gaboandiaalave@gmail.com>
2026-08-13 15:03:56 -0400

    Fix(map) fix map and put all travel data - TravelSearchForm.tsx

--- src/pages/user/TravelSearchForm.tsx
+++ src/pages/user/TravelSearchForm.tsx
@@ -2,8 +2,19 @@ import { useEffect, useState } from "react";
 import { getStops } from "../../services/stopService";
 import { getTravels } from "../../services/travelService";
 import { useLocation } from "react-router-dom";
-import { MapContainer, TileLayer, GeoJSON} from "react-leaflet";
+import { MapContainer, TileLayer, GeoJSON, useMap } from "react-leaflet";
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+function MapAutoZoom({routeData}: { routeData: any}) {
+    const map = useMap();
+    useEffect(() => {
+        if (routeData) {
+            const layer = L.geoJSON(routeData);
+            map.fitBounds(layer.getBounds(), {padding: [50,50]})
+        }
+    }, [routeData, map]);
+    return null;
+}
 function TravelSearchForm() {
     const location = useLocation();
     const { ci, full_name} = location.state || {};
@@ -14,7 +25,7 @@ function TravelSearchForm() {
         id_destiny_stop: "",
         departure_date: ""
     });
-    const [selectedRoute, setSelectedRoute] = useState<any>(null);
+    const [selectedTravelForMap, setSelectedTravelForMap] = useState<any>(null);
 
     useEffect(() => {
         const fetchStops = async () => {
@@ -35,7 +46,7 @@ function TravelSearchForm() {
 
     const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        setSelectedRoute(null);
+        setSelectedTravelForMap(null);
         try {
             const allTravels = await getTravels();
 
@@ -59,7 +70,7 @@ function TravelSearchForm() {
             console.error("Error to search travels", error);
         }
     };
-    console.log("DATOS DE LA RUTA SELECCIONADA:", selectedRoute);
+    
 
     return (
         <>
@@ -132,19 +143,22 @@ function TravelSearchForm() {
                                 Date: {travel.departure_date} <br />
                                 Departure Time: {travel.schedule?.departure_time} <br />
                                 Estimated Arrival time: {travel.schedule?.estimated_arrival_time}
-                                <button onClick={() => setSelectedRoute(travel.route)}>
+                                <button onClick={() => setSelectedTravelForMap(travel)}>
                                     VIEW ROUTE
                                 </button>
+                                <button></button>
                             </li>
                         ))}
                     </ul>
                 </div>
             )}
-            {selectedRoute && (
+            {selectedTravelForMap && (
                 <div style={{ marginTop: '30px', border: '2px solid #ccc', padding: '10px' }}>
                     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                         <h3>Travel Map</h3>
-                        <button onClick={() => setSelectedRoute(null)} style={{ background: 'red', color: 'white' }}>
+                        <p>This is the route on the travel</p>
+                        <p>Estimated travel time: {selectedTravelForMap.schedule?.estimated_travel_time} </p>
+                        <button onClick={() => setSelectedTravelForMap(null)} style={{ background: 'red', color: 'white' }}>
                             ✖ Close Map
                         </button>
                     </div>
@@ -160,10 +174,11 @@ function TravelSearchForm() {
                                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             />
                             <GeoJSON 
-                                key={JSON.stringify(selectedRoute)} 
-                                data={selectedRoute} 
+                                key={JSON.stringify(selectedTravelForMap.route)} 
+                                data={selectedTravelForMap.route} 
                                 style={{ color: 'blue', weight: 5 }} 
                             />
+                            <MapAutoZoom routeData={selectedTravelForMap.route}/>
                         </MapContainer>
                     </div>
                 </div>
